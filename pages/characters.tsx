6e6a09c48b016bb08eca908762575d474a25cfd4
guilhermyr87 <guilhermyr87@gmail.com>
2025-04-30 16:20:43 +0000

    Criando componente CharactersTable e page Characters

---
+++ pages/characters.tsx
@@ -0,0 +1,10 @@
+import CharactersTable from "@/components/CharactersTable";
+
+export default function PageCharacters() {
+    return (
+        <div>
+            <h1>Characters List</h1>
+            <CharactersTable></CharactersTable>
+        </div>
+    )
+}
